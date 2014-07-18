26ce6b6b30454cde0fde0a4f4e3990d8f1b88144
P. Isaac Hildebrandt <isaac@pihimedia.com>
2014-07-18 09:54:31 -0500

    add eventing to features to allow for better performance in branch lookups

--- NFeatureGate/NFeature.cs
+++ NFeatureGate/NFeature.cs
@@ -4,11 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NFeatureGate.Contracts;
+using NFeatureGate.Events;
 
 namespace NFeatureGate
 {
     public class NFeature
     {
-        public string Name { get; set; }
+        public event NFeatureChangedEventHandler NameChanged; 
+
+        private string _name;
+
+        public string Name 
+        { 
+            get 
+            { 
+                return _name; 
+            }
+ 
+            set 
+            {
+                var args = new NFeatureChangedEventArgs(this);
+                _name = value.Trim();
+                OnNameChanged(args);
+            } 
+        }
+
+        protected virtual void OnNameChanged(NFeatureChangedEventArgs args)
+        {
+            if (NameChanged != null) NameChanged(this, args);
+        }
     }
 }
